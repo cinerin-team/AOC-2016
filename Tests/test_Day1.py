b3db93f3ef0103a9261580aa25c8589ff171cd88
Miklos Parrag <miklos.parrag@ericsson.com>
2024-05-03 15:51:15 +0200

    day1

--- Tests/test_Day1.py
+++ Tests/test_Day1.py
@@ -6,13 +6,31 @@ from Days.Day1 import Day1
 class TestDay1(TestCase):
     def test_task1(self):
         expected_value = str(5)
-        actual_value = Day1('../Resources/Day2/test1-1').task1()
+        actual_value = Day1('../Resources/Day1/test1-1').task1()
         self.assertEqual(expected_value, actual_value)
 
         expected_value = str(2)
-        actual_value = Day1('../Resources/Day2/test1-2').task1()
+        actual_value = Day1('../Resources/Day1/test1-2').task1()
         self.assertEqual(expected_value, actual_value)
 
         expected_value = str(12)
-        actual_value = Day1('../Resources/Day2/test1-3').task1()
+        actual_value = Day1('../Resources/Day1/test1-3').task1()
+        self.assertEqual(expected_value, actual_value)
+
+    def test_task2(self):
+        expected_value = str(4)
+        actual_value = Day1('../Resources/Day1/test2').task2()
+        self.assertEqual(expected_value, actual_value)
+
+        expected_value = str(8)
+        actual_value = Day1('../Resources/Day1/test2-2').task2()
+        self.assertEqual(expected_value, actual_value)
+
+    def test_orig(self):
+        expected_value = str(231)
+        actual_value = Day1('../Resources/Day1/input').task1()
+        self.assertEqual(expected_value, actual_value)
+
+        expected_value = str(147)
+        actual_value = Day1('../Resources/Day1/input').task2()
         self.assertEqual(expected_value, actual_value)
--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from Days.Day1 import Day1
 if __name__ == '__main__':
     d1 = Day1('Resources/Day1/input')
     print("day1 task1: " + d1.task1())
-    prin/t("day1 task2: " + d1.task2())
+    print("day1 task2: " + d1.task2())

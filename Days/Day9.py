4c667c8df798e13c661a7d0467386ed3050f5685
Miklos Parrag <miklos.parrag@ericsson.com>
2024-08-02 15:56:12 +0200

    day9 task2 unfinished

--- Days/Day9.py
+++ Days/Day9.py
@@ -1,17 +1,67 @@
-from Utilities.read_file_to_string_array import read_to_string_array
+import six
+
+from Utilities.read_file_to_string import read_to_string
 
 
 class Day9:
+    input_string = ""
 
     def __init__(self, file):
-        self.input_array = read_to_string_array(file)
+        self.input_string = read_to_string(file)
 
     def task1(self):
-        result = 0
+        result = ""
+        index = 0
+        while index < len(self.input_string):
+            if self.input_string[index] != "(":
+                result += self.input_string[index]
+                index += 1
+            else:
+                index += 1
+                mennyit = ""
+                mennyivel = ""
+                while self.input_string[index] != "x":
+                    mennyit += self.input_string[index]
+                    index += 1
+                index += 1
+                while self.input_string[index] != ")":
+                    mennyivel += self.input_string[index]
+                    index += 1
+                index += 1
+                mit = self.input_string[index:index+int(mennyit)]
+                result += (mit * int(mennyivel))
+                index += len(mit)
 
-        return str(result)
+        return str(len(result))
 
     def task2(self):
-        result = 0
+        result = ""
+        uj_input = self.input_string
+        while "(" in uj_input:
+            index = 0
+            result = ""
+            while index < len(uj_input):
+                if uj_input[index] != "(":
+                    result += uj_input[index]
+                    index += 1
+                else:
+                    index += 1
+                    mennyit = ""
+                    mennyivel = ""
+                    while uj_input[index] != "x":
+                        mennyit += uj_input[index]
+                        index += 1
+                    index += 1
+                    while uj_input[index] != ")":
+                        mennyivel += uj_input[index]
+                        index += 1
+                    index += 1
+                    mit = uj_input[index:index+int(mennyit)]
+                    result += (mit * int(mennyivel))
+                    index += len(mit) * int(mennyivel) + 1
 
-        return str(result)
+            uj_input = result
+            continue
+
+        print(result)
+        return str(len(result))
--- Tests/test_Day9.py
+++ Tests/test_Day9.py
@@ -29,16 +29,25 @@ class TestDay9(TestCase):
         actual_value = Day9('../Resources/Day9/test6').task1()
         self.assertEqual(expected_value, actual_value)
 
-    # def test_task2(self):
-    #     expected_value = str('5DB3')
-    #     actual_value = Day8('../Resources/Day8/test1-1').task2()
-    #     self.assertEqual(expected_value, actual_value)
+    def test_task2(self):
+        expected_value = str('9')
+        actual_value = Day9('../Resources/Day9/test3').task2()
+        self.assertEqual(expected_value, actual_value)
 
-    # def test_orig(self):
-    #     pass
-    #     expected_value = str(119)
-    #     actual_value = Day8('../Resources/Day8/input').task1()
-    #     self.assertEqual(expected_value, actual_value)
+        # expected_value = str('241920')
+        # actual_value = Day9('../Resources/Day9/test7').task2()
+        # self.assertEqual(expected_value, actual_value)
+
+        expected_value = str('445')
+        actual_value = Day9('../Resources/Day9/test8').task2()
+        self.assertEqual(expected_value, actual_value)
+
+
+    def test_orig(self):
+        pass
+        expected_value = str(70186)
+        actual_value = Day9('../Resources/Day9/input').task1()
+        self.assertEqual(expected_value, actual_value)
     #     #
     #     expected_value = str('C2C28')
     #     actual_value = Day8('../Resources/Day8/input').task2()
